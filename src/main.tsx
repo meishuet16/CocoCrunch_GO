2176f7e931c5e72f505b4ffc7720d9753edb6354
meishuet <meishuetlee8@gmail.com>
2026-09-03 21:35:55 +0800

    feat: mount centralized Coco experience layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import AppExperience from './AppExperience';
 import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppExperience />
   </React.StrictMode>,
 );
