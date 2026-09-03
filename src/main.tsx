d8653d5dd9a2dca31cfbf2bb706f6891307d68db
meishuet <meishuetlee8@gmail.com>
2026-09-03 21:29:15 +0800

    feat: mount Coco signature rituals

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import SignatureRituals from './SignatureRituals';
 import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
@@ -8,5 +9,6 @@ import './signature-interactions.css';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SignatureRituals />
   </React.StrictMode>,
 );
