e4b1fdbbdfc48f4385a35ed82328e7c2514f54aa
meishuet <meishuetlee8@gmail.com>
2026-09-03 21:51:30 +0800

    refactor: keep only integrated domain foundation

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import AppExperience from './AppExperience';
 import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
-import './memories/memory-rituals.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/memories/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MemoryRituals } from './MemoryRituals';
