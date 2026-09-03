dc47bd75c88e6a99e272ffa0f84fb15efd4898d0
meishuet <meishuetlee8@gmail.com>
2026-09-04 02:10:56 +0800

    style: load V2 polish overrides last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
 import './luggage-interactions.css';
+import './v2-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
