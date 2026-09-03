c06083022b85058e6c341fe62babbc3ae3f78777
meishuet <meishuetlee8@gmail.com>
2026-09-03 21:25:52 +0800

    style: load signature interaction layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './coco-character.css';
+import './signature-interactions.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
