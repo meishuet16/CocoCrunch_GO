a21e63ebc3c7dadadfb85a17ec120f7676a38949
meishuet <meishuetlee8@gmail.com>
2026-09-05 23:49:14 +0800

    feat(explore): mount expanded searchable V3 experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppExperience from './AppExperience';
-import ExploreReplicaV2 from './components/explore/ExploreReplicaV2';
+import ExploreReplicaV3 from './components/explore/ExploreReplicaV3';
 import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
@@ -10,11 +10,11 @@ import './v2-polish.css';
 import './gacha-interactions.css';
 import './explore-redesign.css';
 import './explore-content-semantics.css';
-import './explore-replica-v2.css';
+import './explore-replica-v3.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppExperience />
-    <ExploreReplicaV2 />
+    <ExploreReplicaV3 />
   </React.StrictMode>,
 );
