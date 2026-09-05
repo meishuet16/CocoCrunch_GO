38c4937709cdf4b39557bc1bf452c53405ec5089
meishuet <meishuetlee8@gmail.com>
2026-09-05 23:41:39 +0800

    feat(explore): mount refined Explore V2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppExperience from './AppExperience';
-import ExploreReplica from './components/explore/ExploreReplica';
+import ExploreReplicaV2 from './components/explore/ExploreReplicaV2';
 import './styles.css';
 import './coco-character.css';
 import './signature-interactions.css';
@@ -10,11 +10,11 @@ import './v2-polish.css';
 import './gacha-interactions.css';
 import './explore-redesign.css';
 import './explore-content-semantics.css';
-import './explore-replica.css';
+import './explore-replica-v2.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppExperience />
-    <ExploreReplica />
+    <ExploreReplicaV2 />
   </React.StrictMode>,
 );
